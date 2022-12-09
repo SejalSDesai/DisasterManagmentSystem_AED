8b8007289aedbea47fe1a2ece5297006f64e1f55
wadhwa8055 <wadhwa.ya@northeastern.edu>
2022-12-08 19:09:36 -0500

    Implement logout functionality on manageIOrganization and ManageUserAccount

--- src/UI/EventMaker/ManageUserAccount.java
+++ src/UI/EventMaker/ManageUserAccount.java
@@ -102,7 +102,7 @@ public class ManageUserAccount extends javax.swing.JPanel {
         comboEmp = new javax.swing.JComboBox();
         jLabel5 = new javax.swing.JLabel();
         comboRole = new javax.swing.JComboBox();
-        jLabel6 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
 
         setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
@@ -212,9 +212,13 @@ public class ManageUserAccount extends javax.swing.JPanel {
 
         add(jPanel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(6, 190, 620, 446));
 
-        jLabel6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/UI/Images/ezgif.com-gif-maker.png"))); // NOI18N
-        jLabel6.setText("jLabel6");
-        add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(1020, 10, 110, 100));
+        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/UI/Images/ezgif.com-gif-maker-3.png"))); // NOI18N
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(1050, 30, 50, 50));
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnAddUserActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAddUserActionPerformed
@@ -257,18 +261,25 @@ public class ManageUserAccount extends javax.swing.JPanel {
         }
     }//GEN-LAST:event_comboOrgActionPerformed
 
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        userProcessContainer.remove(this);
+        CardLayout layout = (CardLayout) userProcessContainer.getLayout();
+        layout.previous(userProcessContainer);
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnAddUser;
     private javax.swing.JComboBox comboEmp;
     private javax.swing.JComboBox comboOrg;
     private javax.swing.JComboBox comboRole;
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
     private javax.swing.JPanel jPanel3;
     private javax.swing.JPanel jPanel4;
     private javax.swing.JScrollPane jScrollPane1;
